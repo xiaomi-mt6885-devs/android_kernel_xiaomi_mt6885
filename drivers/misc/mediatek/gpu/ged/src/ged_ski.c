228cc5d67f1f1ec711136f0c133b5faccadb8533
Rama Bondan Prakoso <ramanarubp@gmail.com>
2021-05-03 12:35:25 +0000

    drivers: misc/mtk: gpu/ged_ski: Change freq info to MHz
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/misc/mediatek/gpu/ged/src/ged_ski.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_ski.c
@@ -44,7 +44,7 @@ ssize_t gpu_clock_show(struct kobject *kobj,
 
 	gpu_freq = mt_gpufreq_get_cur_volt() ? mt_gpufreq_get_cur_freq() : 0;
 
-	return scnprintf(buf, PAGE_SIZE, "%u\n", gpu_freq);
+	return scnprintf(buf, PAGE_SIZE, "%u\n", gpu_freq / 1000);
 }
 
 static KOBJ_ATTR_RO(gpu_clock);
@@ -67,7 +67,7 @@ ssize_t gpu_freq_table_show(struct kobject *kobj,
 
 	for (idx = max_opp_idx; count < table_num; count++) {
 		length = scnprintf(temp + pos, 1024 - pos,
-				"%u ", power_table[idx + count].gpufreq_khz);
+				"%u ", power_table[idx + count].gpufreq_khz / 1000);
 		pos += length;
 	}
 
@@ -95,7 +95,7 @@ static ssize_t gpu_max_clock_show(struct kobject *kobj,
 	max_clock = (max_clock_custom < max_clock) ?
 			max_clock_custom : max_clock;
 
-	return scnprintf(buf, PAGE_SIZE, "%u\n", max_clock);
+	return scnprintf(buf, PAGE_SIZE, "%u\n", max_clock / 1000);
 }
 
 static ssize_t gpu_max_clock_store(struct kobject *kobj,
@@ -149,7 +149,7 @@ ssize_t gpu_min_clock_show(struct kobject *kobj,
 	min_clock = (min_clock_custom > min_clock) ?
 			min_clock_custom : min_clock;
 
-	return scnprintf(buf, PAGE_SIZE, "%lu\n", min_clock);
+	return scnprintf(buf, PAGE_SIZE, "%lu\n", min_clock / 1000);
 }
 
 static ssize_t gpu_min_clock_store(struct kobject *kobj,
