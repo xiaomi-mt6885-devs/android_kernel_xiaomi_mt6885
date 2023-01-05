08285c9777f53b4d2c045dfcc0ce204e307518fc
xiaoleGun <1592501605@qq.com>
2023-01-05 13:03:36 +0800

    drivers/: mtk-conninfra: Invalidate function
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- drivers/misc/mediatek/connectivity/conninfra/drv_init/gps_drv_init.c
+++ drivers/misc/mediatek/connectivity/conninfra/drv_init/gps_drv_init.c
@@ -8,7 +8,7 @@
 #include "gps_drv_init.h"
 
 #ifdef CONFIG_MTK_GPS_SUPPORT
-int __attribute__((weak)) mtk_wcn_gpsdl_drv_init()
+int __attribute__((weak)) mtk_wcn_gpsdl_drv_init(void)
 {
 	pr_info("No impl. %s\n", __func__);
 	return 0;
