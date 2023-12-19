fced809c98ad708a26c700cb25defc88ed230841
Tkpointz <pointzztk@gmail.com>
2023-12-19 15:57:28 +0800

    soc: mtk: Fix incompatible pointer to integer conversion
    error: incompatible integer to pointer conversion returning 'int' from a function with result type 'void *' [-Wint-conversion]
                            return -ENOMEM;
                                   ^~~~~~~
    Signed-off-by: Tkpointz <pointzztk@gmail.com>

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -596,7 +596,7 @@ void *cmdq_pkt_get_curr_buf_va(struct cmdq_pkt *pkt)
 
 	if (unlikely(!pkt->avail_buf_size))
 		if (cmdq_pkt_add_cmd_buffer(pkt) < 0)
-			return -ENOMEM;
+			return ERR_PTR(-ENOMEM);
 
 	buf = list_last_entry(&pkt->buf, typeof(*buf), list_entry);
 
