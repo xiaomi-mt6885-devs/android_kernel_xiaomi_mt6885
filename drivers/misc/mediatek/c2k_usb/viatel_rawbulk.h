b02c726b32224c2327aadc138844d8c1586b3992
cool <cool@gayhub.eu.org>
2023-12-19 15:57:27 +0800

    drivers: misc/mtk: c2k_usb: Use unsigned type for 1-bit bitfields
    drivers/misc/mediatek/c2k_usb/f_rawbulk.c:345:16: error: implicit truncation f
    rom 'int' to a one-bit wide bit-field changes value from 1 to -1 [-Werror,-Wsingle-bit-bitfield-constant-conversion]
            fn->activated = 1;
                          ^ ~
    drivers/misc/mediatek/c2k_usb/f_rawbulk.c:390:19: error: implicit truncation f
    rom 'int' to a one-bit wide bit-field changes value from 1 to -1 [-Werror,-Wsingle-bit-bitfield-constant-conversion]
                    fn->initialized = 1;
                                    ^ ~
    drivers/misc/mediatek/c2k_usb/f_rawbulk.c:470:19: error: implicit truncation f
    rom 'int' to a one-bit wide bit-field changes value from 1 to -1 [-Werror,-Wsingle-bit-bitfield-constant-conversion]
                    fn->initialized = 1;
                                    ^ ~
    3 errors generated.

--- drivers/misc/mediatek/c2k_usb/viatel_rawbulk.h
+++ drivers/misc/mediatek/c2k_usb/viatel_rawbulk.h
@@ -76,11 +76,11 @@ struct rawbulk_function {
 
 	/* Controls */
 	spinlock_t lock;
-	int enable:1;
-	int activated:1;	/* set when usb enabled */
-	int tty_opened:1;
+	unsigned int enable:1;
+	unsigned int activated:1;	/* set when usb enabled */
+	unsigned int tty_opened:1;
 
-	int initialized:1;	/* init-flag for activator worker */
+	unsigned int initialized:1;	/* init-flag for activator worker */
 	struct work_struct activator;	/* asynic transaction starter */
 
 	struct wakeup_source keep_awake;
